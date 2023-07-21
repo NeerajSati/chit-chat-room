87c46b8628af7bb388776b424f699694de31dd75
Neeraj Sati <neeraj.sati123@gmail.com>
2023-07-21 15:08:25 +0530

    Register redux reducer added

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -2,32 +2,83 @@ import React, {useState} from 'react'
 import {RxCross2} from 'react-icons/rx'
 import {AiFillEye, AiFillEyeInvisible} from 'react-icons/ai'
 import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux'
-import authActions from '../../redux/authSlice'
+import { useDispatch } from 'react-redux';
+import {registerUser} from '../../redux/authSlice';
+import { toast } from 'react-toastify';
 
 function Register({switchPage}) {
     const [showPassword, setShowPassword] = useState(false);
+    const [disableAuthActions,setDisableAuthActions] = useState(false);
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
     const dispatch = useDispatch()
+
+    const validateRegisterData = () =>{
+        if(!email){
+            toast.error('Please enter email!',{autoClose: 2000, theme: "dark"});
+            return true;
+        }
+        if(!username){
+            toast.error('Please enter username!',{autoClose: 2000, theme: "dark"});
+            return true;
+        }
+        if(!password){
+            toast.error('Please enter password!',{autoClose: 2000, theme: "dark"});
+            return true;
+        }
+
+        const regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+        if(!regex.test(email)){
+            toast.error('This email is invalid!',{autoClose: 2000, theme: "dark"});
+            return true;
+        }
+    }
+
+    const registerUserInit = async() => {
+        try{
+            if(validateRegisterData()){
+                return;
+            }
+            setDisableAuthActions(true);
+            const formContent = new FormData();
+            formContent.append("email",email)
+            formContent.append("username",username)
+            formContent.append("password",password)
+            await dispatch(registerUser({formContent}))
+            setDisableAuthActions(false);
+        } catch(err){
+            setDisableAuthActions(false);
+            console.log(err)
+        }
+    }
 
   return (
         <div className='bg-[#2f2f2f] py-7 px-10 flex flex-col items-center justify-center animate__animated animate__flipInY animate__faster max-sm:w-full max-sm:px-5'>
             <div className='text-[25px] max-sm:text-[20px]'>Create an Account!</div>
             <div className='mt-5 flex flex-col items-start justify-start max-sm:w-full'>
                 <div className='text-[14px]'>Email</div>
-                <input className='rounded-sm mt-2 py-3 px-5 bg-[#1c1c1c] text-[gray] w-[450px] max-sm:w-full'/>
+                <input
+                    onChange={(e) => setEmail(e.target.value)}
+                    className='rounded-sm mt-2 py-3 px-5 bg-[#1c1c1c] text-[gray] w-[450px] max-sm:w-full'/>
             </div>
             <div className='mt-5 flex flex-col items-start justify-start max-sm:w-full'>
                 <div className='text-[14px]'>Username</div>
-                <input className='rounded-sm mt-2 py-3 px-5 bg-[#1c1c1c] text-[gray] w-[450px] max-sm:w-full'/>
+                <input
+                    onChange={(e) => setUsername(e.target.value)} 
+                    className='rounded-sm mt-2 py-3 px-5 bg-[#1c1c1c] text-[gray] w-[450px] max-sm:w-full'/>
             </div>
             <div className='mt-5 flex flex-col items-start justify-start relative max-sm:w-full'>
                 <div className='text-[14px]'>Password</div>
-                <input type={showPassword ? "text" : "password"} className='rounded-sm mt-2 py-3 pr-10 px-5 bg-[#1c1c1c] text-[gray] w-[450px] tracking-wider max-sm:w-full'/>
+                <input 
+                    onChange={(e) => setPassword(e.target.value)}
+                    type={showPassword ? "text" : "password"} 
+                    className='rounded-sm mt-2 py-3 pr-10 px-5 bg-[#1c1c1c] text-[gray] w-[450px] tracking-wider max-sm:w-full'/>
                 <div onClick={()=>setShowPassword(visible => !visible)} className='absolute bottom-[14px] right-3 cursor-pointer text-[20px]'>
                 {showPassword ? <AiFillEye/> : <AiFillEyeInvisible/>}
                 </div>
             </div>
-            <button className='w-full rounded-sm mt-5 py-2 px-2 bg-[#006FA2]'>Continue</button>
+            <button disabled={disableAuthActions} onClick={registerUserInit} className='w-full rounded-sm mt-5 py-2 px-2 bg-[#006FA2] disabled:opacity-50 disabled:cursor-not-allowed'>Continue</button>
             <div onClick={()=>switchPage('Login')} className='cursor-pointer mt-5 text-[#13A67D]'>Already have an account?</div>
             <Link to="/" className='cursor-pointer absolute right-[2rem] top-[1.5rem] bg-[#ff000040] p-3 rounded-full text-[22px] max-sm:text-[16px] max-sm:right-[1rem]'><RxCross2/></Link>
         </div>
--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -23,6 +23,31 @@ export const loginUser = createAsyncThunk(
       }
   }
 );
+
+export const registerUser = createAsyncThunk(
+  "auth/register",
+  async (payload, thunkAPI) => {
+      const {formContent} = payload
+      console.log(payload)
+      try {
+        const userAuth = await axios.post(registerAPI(),formContent, {
+          headers: {
+            "content-type": "multipart/form-data",
+          }
+        });
+        toast.success("Successfully Registered! Logging you in...")
+        return userAuth.data;
+      } catch (err) {
+        if(err.response.data.msg){
+          toast.error(err.response.data.msg);
+        } else{
+          toast.error("Something went wrong");
+        }
+        throw err;
+      }
+  }
+);
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState: {
@@ -43,20 +68,16 @@ export const authSlice = createSlice({
     })
     .addCase(loginUser.rejected, (state, action) => {
       throw action.error;
-    });
-      // .addCase(registerUser.pending, (state, action) => {
-      // })
-    //   .addCase(registerUser.fulfilled, (state, action) => {
-    //     // state.authToken = action.payload.token;
-    //     // storeAccessToken(action?.payload?.token);
-    //   })
-    //   .addCase(registerUser.rejected, (state, action) => {
-    //     throw action.error;
-    //   })
+    })
+    .addCase(registerUser.pending, (state, action) => {
+    })
+    .addCase(registerUser.fulfilled, (state, action) => {
+      state.authToken = action.payload.token;
+    })
+    .addCase(registerUser.rejected, (state, action) => {
+      throw action.error;
+    })
   },
 })
-
-// Action creators are generated for each case reducer function
-export const {registerUser} = authSlice.actions
 
 export default authSlice.reducer
