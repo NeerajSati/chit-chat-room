3c35026fb93d7d0fd86389c6b398705218dd0022
Neeraj Sati <neeraj.sati123@gmail.com>
2023-07-23 12:47:42 +0530

    Create group Modal designed

---
+++ src/components/CreateGroup/CreateGroup.jsx
@@ -0,0 +1,131 @@
+import React, { useRef, useState } from 'react'
+import {RiImageAddFill} from 'react-icons/ri'
+import {FaSearch} from 'react-icons/fa';
+import {MdOutlineRemoveCircle} from 'react-icons/md';
+
+function CreateGroup() {
+    const [uploadedFile,setUploadedFile] = useState("")
+    const [groupName, setGroupName] = useState("");
+    const [groupDescription, setGroupDescription] = useState("");
+    const [searchUsername, setSearchUsername] = useState("");
+    const [selectedMembersList, setSelectedMembersList] = useState([])
+    const [searchedMembersList, setSearchedMembersList] = useState([]);
+    const inputFile = useRef();
+
+    const imageUploadHandler = (e) => {
+        if(e.target.files[0]){
+            setUploadedFile(URL.createObjectURL(e.target.files[0]))
+        }
+    }
+
+    const searchUsernameHandler = () =>{
+        if(searchUsername){
+
+        }
+    }
+
+    const selectUserHandler = (memberId, username, makeAdmin) =>{
+        if(memberId){
+            setSelectedMembersList((list)=>{
+                return [...list, {userId: memberId, username, isAdmin: makeAdmin}]
+            })
+        }
+    }
+    const selectUserRemoveHandler = (memberId) =>{
+        if(memberId){
+            setSelectedMembersList((list)=>{
+                const arr = list.filter((el) => el.userId !== memberId);
+                return [...arr]
+            });
+        }
+    }
+
+  return (
+    <div onClick={(e)=>{e.stopPropagation()}} className='py-2 px-2 w-[600px] max-h-[90vh] max-md:w-screen max-md:h-screen max-md:max-h-screen bg-[#ffffff] overflow-y-auto'>
+        <div className='py-2 pb-5 font-bold w-full text-center text-[20px]'>Create a group!</div>
+        <div className='grid grid-cols-6'>
+            <div className='col-start-1 col-end-3 flex items-center justify-center'>
+                {
+                    uploadedFile ? (<img alt="profileImage" 
+                    onClick={()=>{inputFile.current.click();}}
+                    className='cursor-pointer rounded-full border-2 border-gray-500 w-[150px] h-[150px] max-md:w-[90px] max-md:h-[90px]' 
+                    onError={(e)=>{e.target.onerror = null; e.target.src=process.env.REACT_APP_FALLBACK_IMAGE}}
+                    src={uploadedFile}
+                    ></img>) : (
+                        <div onClick={()=>{inputFile.current.click();}} className='cursor-pointer rounded-full w-[150px] h-[150px] max-md:w-[60px] max-md:h-[60px] bg-gray-700 flex items-center justify-center text-white text-[100px]'>
+                            <RiImageAddFill/>
+                        </div>
+                    )
+                }  
+            </div>
+            <div className='col-start-3 col-end-7 flex flex-col items-start justify-center'>
+                <div className='py-2 font-semibold'>Enter Group Name: 
+                    <input value={groupName} onChange={(e)=>{setGroupName(e.target.value)}} className='ml-1 border-b-2 border-gray-300 outline-none font-normal' type="text" placeholder='DSA Discussion'></input>
+                </div>
+                <div className='py-2 font-semibold'>Enter Group Description: 
+                    <input value={groupDescription} onChange={(e)=>{setGroupDescription(e.target.value)}} className='ml-1 border-b-2 border-gray-300 outline-none font-normal' type="text" placeholder='Any description here...'></input>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div className='px-5 pt-5 font-semibold'>Search members:
+                <div className='mt-2 w-full h-[50px] rounded-lg px-4 pr-2 py-2 flex flex-row justify-between items-center border-2 border-gray-700'>
+                    <input placeholder='Type username!' 
+                    value={searchUsername} 
+                    onChange={(e)=>{setSearchUsername(e.target.value)}} 
+                    onKeyDown={(e) => {if (e.key === "Enter") {searchUsernameHandler()}}}
+                    className='outline-none w-[90%] pr-5'></input>
+                    <div onClick={searchUsernameHandler} className='bg-[#09794a] cursor-pointer w-[40px] h-[40px] rounded-full flex items-center justify-center mr-3'>
+                        <FaSearch className='text-[25px] text-white'/>
+                    </div>
+                </div>
+            </div>
+            <div className='w-full bg-white rounded-md px-5 searchedMemberList max-h-[130px] overflow-y-auto'>
+                {
+                    searchedMembersList && searchedMembersList.map((member)=>{
+                        if(selectedMembersList.find((selected)=>selected.userId === member._id)){
+                            return null;
+                        }
+                        return <div className='w-full h-[45px] bg-[#e2e3e397] border-2 border-gray-300 mt-2 rounded-sm px-2 flex flex-row items-center justify-between' key={member._id}>
+                            <div className='flex flex-row items-center justify-center'>
+                                <img alt="profileImage" 
+                                    onClick={()=>{inputFile.current.click();}}
+                                    className='rounded-full border-2 border-gray-500 w-[40px] h-[40px]' 
+                                    onError={(e)=>{e.target.onerror = null; e.target.src=process.env.REACT_APP_FALLBACK_IMAGE}}
+                                    src={uploadedFile}
+                                ></img>
+                                <div className='pl-2 font-bold'>@{member.username}</div>
+                            </div>
+                            <div className='flex flex-row items-center justify-center'>
+                                <button onClick={()=>{selectUserHandler(member._id, member.username, false)}} className='px-2 bg-blue-300 rounded-sm ml-3 text-[14px] font-semibold'>Add</button>
+                                <button onClick={()=>{selectUserHandler(member._id, member.username, true)}} className='px-2 bg-[#84e881] rounded-sm ml-3 text-[14px] font-semibold'>Admin</button>
+                            </div>
+                        </div>
+                    })
+                }
+            </div>
+        </div>
+        
+        {
+        selectedMembersList && selectedMembersList.length ?
+            <div className='px-5 pt-6 font-semibold'>Selected members:</div> : <></>
+        }
+        <div className='px-5 searchedMemberList overflow-y-auto flex flex-row flex-wrap'>
+            {
+                selectedMembersList && selectedMembersList.map((member)=>{
+                    return <div onClick={()=>{selectUserRemoveHandler(member.userId)}} style={{backgroundColor:member.isAdmin ? '#b4e1b4' : '#e2e3e397'}} className='cursor-pointer h-[35px] mr-2 border-2 border-gray-300 mt-2 rounded-sm px-1 flex flex-row items-center justify-center' key={member._id}>
+                            <div className='font-semibold text-[14px]'>@{member.username}</div>
+                            <button className='text-[#ce2727] mt-1 ml-2'><MdOutlineRemoveCircle/></button>
+                    </div>
+                })
+            }
+        </div>
+        <div className='w-full flex items-center justify-center mt-2'>
+            <button onClick={createGroupHandler} className='bg-[#09794A] text-white px-4 py-1 rounded-md'>Create Group</button>
+        </div>
+        <input type='file' id='file' accept='image/*' ref={inputFile} onChange={imageUploadHandler} style={{display: 'none'}}/>
+    </div>
+  )
+}
+
+export default CreateGroup
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -9,6 +9,7 @@ import ActiveChat from '../ActiveChat/ActiveChat';
 import { useDispatch, useSelector } from 'react-redux'
 import {chatActions} from '../../redux/chatSlice'
 import {socket} from '../utils/socket'
+import CreateGroup from '../CreateGroup/CreateGroup';
 
 function Dashboard() {
   const chatList = useSelector((state) => state.chat.chats);
@@ -17,6 +18,7 @@ function Dashboard() {
   const dispatch = useDispatch();
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedChats, setSelectedChats] = useState([]);
+  const [viewCreateGroupModal, setViewCreateGroupModal] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -61,42 +63,51 @@ function Dashboard() {
   }
 
   return (
-    <div className='flex flex-row w-full h-screen'>
-      <div className='flex-[3.5] bg-[#2B2D31] w-full flex flex-col'>
-        <div className='h-[70px] w-full bg-[#111213] flex items-center justify-between'>
-          <div className='w-[50px] h-[50px] ml-2 relative cursor-pointer max-md:w-[40px] max-md:h-[40px]'>
-            <img alt="profileImage" 
-            className='w-full h-full rounded-full' 
-            onError={(e)=>{e.target.onerror = null; e.target.src=process.env.REACT_APP_FALLBACK_IMAGE}}
-            src="https://images.pexels.com/photos/17102422/pexels-photo-17102422/free-photo-of-wood-landscape-water-summer.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            ></img>  
-            <div className='absolute bottom-[-8px] right-[-8px] w-5 h-5 bg-green-400 flex items-center justify-center rounded-lg'>
-              <BiMessageSquareAdd/>
+    <>
+      <div className='flex flex-row w-full h-screen'>
+        <div className='flex-[3.5] bg-[#2B2D31] w-full flex flex-col'>
+          <div className='h-[70px] w-full bg-[#111213] flex items-center justify-between'>
+            <div className='w-[50px] h-[50px] ml-2 relative cursor-pointer max-md:w-[40px] max-md:h-[40px]'>
+              <img alt="profileImage" 
+              className='w-full h-full rounded-full' 
+              onError={(e)=>{e.target.onerror = null; e.target.src=process.env.REACT_APP_FALLBACK_IMAGE}}
+              src="https://images.pexels.com/photos/17102422/pexels-photo-17102422/free-photo-of-wood-landscape-water-summer.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              ></img>  
+              <div className='absolute bottom-[-8px] right-[-8px] w-5 h-5 bg-green-400 flex items-center justify-center rounded-lg'>
+                <BiMessageSquareAdd/>
+              </div>
+            </div>
+            <div className='h-[70px] flex items-center mr-2'>
+              <AiOutlineUsergroupAdd onClick={()=>{setViewCreateGroupModal(true)}} className='text-[25px] mr-4 text-gray-300 cursor-pointer'/>
+              <SlOptionsVertical className='text-[20px] text-gray-300 cursor-pointer'/>
             </div>
           </div>
-          <div className='h-[70px] flex items-center mr-2'>
-            <AiOutlineUsergroupAdd className='text-[25px] mr-4 text-gray-300 cursor-pointer'/>
-            <SlOptionsVertical className='text-[20px] text-gray-300 cursor-pointer'/>
+          <div className='py-3 flex items-center justify-center'>
+            <div className='bg-[#18191b] text-[15px] text-white rounded-md p-2 py-2 w-[90%] flex flex-row items-center cursor-pointer'>
+              {
+                searchTerm ? 
+                <BiArrowBack className='text-[20px] text-green-400' onClick={() => setSearchTerm("")}/> :
+                <FaSearch onClick={() => setSearchTerm("")}/>
+              }
+              <input placeholder='Search my chats' value={searchTerm} onChange={(e)=>{setSearchTerm(e.target.value)}} className='bg-[#18191b] outline-none w-full ml-4'/>
+            </div>
+          </div>
+          <div className='overflow-y-scroll'>
+            <Chatlist chatList={selectedChats}/>
           </div>
         </div>
-        <div className='py-3 flex items-center justify-center'>
-          <div className='bg-[#18191b] text-[15px] text-white rounded-md p-2 py-2 w-[90%] flex flex-row items-center cursor-pointer'>
-            {
-              searchTerm ? 
-              <BiArrowBack className='text-[20px] text-green-400' onClick={() => setSearchTerm("")}/> :
-              <FaSearch onClick={() => setSearchTerm("")}/>
-            }
-            <input placeholder='Search my chats' value={searchTerm} onChange={(e)=>{setSearchTerm(e.target.value)}} className='bg-[#18191b] outline-none w-full ml-4'/>
+        <div className='flex-[6.5] bg-[#323338] w-full'>
+          <ActiveChat/>
+        </div>
+      </div>
+        {
+          viewCreateGroupModal && (
+            <div onClick={()=>{setViewCreateGroupModal(false)}} className='fixed top-0 right-0 left-0 bottom-0 w-screen h-screen bg-[#173a2963] flex items-center justify-center'>
+              <CreateGroup />
           </div>
-        </div>
-        <div className='overflow-y-scroll'>
-          <Chatlist chatList={selectedChats}/>
-        </div>
-      </div>
-      <div className='flex-[6.5] bg-[#323338] w-full'>
-        <ActiveChat/>
-      </div>
-    </div>
+        )
+      }
+    </>
   )
 }
 
